57c7ad5c53cf6ed3d2cf0907d56b8bf3c84f57ba
Juan Sanchez <juansanchez721@outlook.com>
2021-01-23 15:37:03 -0500

    Added to splashpage

--- frontend/components/search/searchbar.jsx
+++ frontend/components/search/searchbar.jsx
@@ -5,8 +5,18 @@ import React from 'react'
 const SearchBar = () => {
 
     return (
+        
         <div className="searchbar-container">
-            <input className="search-bar" placeholder= "search artist or song"/>
+            <div className="innersearchcontainer">
+
+            <div className="search-first">
+                <input className="search-bar" placeholder= "search artist or song"/>
+            </div>
+            <div className="or-upload" >
+                <p>or</p>
+                <button className="orangebutton"> Upload your own</button>
+            </div>
+            </div>
         </div>
     )
 }
