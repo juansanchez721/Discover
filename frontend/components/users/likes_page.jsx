ff9b71a062e61b5236162a4535012eca07f509c8
Juan Sanchez <juansanchez721@outlook.com>
2021-03-15 21:12:44 -0400

    working on artist likes

--- frontend/components/users/likes_page.jsx
+++ frontend/components/users/likes_page.jsx
@@ -1,55 +1,68 @@
-import React from 'react'
-import DiscoverPageItem from '../discover_page/discover_page_item'
+import React from "react";
+import DiscoverPageItem from "../discover_page/discover_page_item";
+import { NavLink, Switch } from "react-router-dom";
 
-class LikesPage extends React.Component { 
-// const LikesPage = (props) => {
+class LikesPage extends React.Component {
+  // const LikesPage = (props) => {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            loaded: true
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: true,
+    };
+  }
 
-    // console.log(props.tracks)
-    
-    componentDidMount() {
- 
-        this.props.fetchTrackLikes(this.props.match.params.userId)
-        .then(() => this.setState({ loaded: false }) ) 
-    }
+  // console.log(props.tracks)
 
-    render() {
+  componentDidMount() {
+    this.props
+      .fetchTrackLikes(this.props.match.params.userId)
+      .then(() => this.setState({ loaded: false }));
+  }
 
-        const {likedTracks} = this.props 
-        if(this.state.loaded) return null;
-        
-        let likeTracks = Object.values(likedTracks).map(track => (
-            <DiscoverPageItem
-             key={track.id} 
-             track={track}
-             liked={track.likers.includes(this.props.currentUser.id)}
-             createLike={this.props.createLike}
-            deleteLike={this.props.deleteLike}
-             /> 
-            
-            ))
-            
-            return (
-                <div className="likes-container" >
-                    <div className='likes-header'>
+  render() {
+    const { likedTracks } = this.props;
+    if (this.state.loaded) return null;
 
-                    <h1 >
-                        hear the tracks you've liked:
-                    </h1>
-                    </div>
-            <div className="likes-inner" >
-            {likeTracks}
+    let likeTracks = Object.values(likedTracks).map((track) => (
+      <DiscoverPageItem
+        key={track.id}
+        track={track}
+        liked={track.likers.includes(this.props.currentUser.id)}
+        createLike={this.props.createLike}
+        deleteLike={this.props.deleteLike}
+      />
+    ));
+
+    return (
+      <div className="likes-container">
+        <div className="profile-comments-top">
+            <div className="profile-navlinks" >
+
+          <NavLink
+            exact
+            to={`/users/${this.props.match.params.userId}/comments`}
+            activeStyle={{
+                fontWeight: "bold",
+                color: "#FE5000",
+            }}>
+            Comments
+          </NavLink>
+          <NavLink
+            exact
+            to={`/users/${this.props.match.params.userId}/likes`}
+            activeStyle={{
+                fontWeight: "bold",
+                color: "#FE5000",
+            }}>
+            likes
+          </NavLink>
             </div>
         </div>
-    )
-}
+        <div className="likes-inner">{likeTracks}</div>
+      </div>
+    );
+  }
 }
 
-
-export default LikesPage
+export default LikesPage;
--- frontend/components/users/profile_comments.jsx
+++ frontend/components/users/profile_comments.jsx
@@ -1,6 +1,9 @@
 import React from 'react' 
+import { NavLink, Switch } from 'react-router-dom'
 import ProfileCommentItem from './profile_comments_item'
-
+import LikesContainer from '../../components/users/likes_container'
+import ProfileCommentsContainer from '../../components/users/profile_comments_container'
+import { AuthRoute, ProtectedRoute } from '../../util/route_util';
 
 
 class ProfileComments extends React.Component {
@@ -25,8 +28,21 @@ class ProfileComments extends React.Component {
             <div className="profile-comments-page" >
                 <div  className="profile-comments-top">
                     <h1>here commente shere</h1>
-
+                <NavLink exact to={`/users/${this.props.match.params.userId}/comments`}  activeStyle={{
+                      fontWeight: "bold",
+                      color: "#FE5000"
+                    }}>Comments</NavLink>
+                    <NavLink exact to={`/users/${this.props.match.params.userId}/likes`}  activeStyle={{
+                      fontWeight: "bold",
+                      color: "#FE5000"
+                    }}>likes</NavLink>
                 </div>
+                {/* <Switch>
+                    
+                <ProtectedRoute exact path="/users/:userId/likes" component={LikesContainer}/>
+                <ProtectedRoute exact path="/users/:userId/comments" component={ProfileCommentsContainer}/>
+                </Switch> */}
+
             {comments}
         </div>
     )
