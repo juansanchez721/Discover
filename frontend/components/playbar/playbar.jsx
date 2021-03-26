d42e5ec7466f6f357a3a96a354d97525bc0fab88
Juan Sanchez <juansanchez721@outlook.com>
2021-03-26 17:55:00 -0400

    slider changes current time

--- frontend/components/playbar/playbar.jsx
+++ frontend/components/playbar/playbar.jsx
@@ -1,150 +1,174 @@
-import React from 'react'
+import React from "react";
 // import song from '../../../app/assets/audio/Skeleton.mp3'
 // const song = require('../../../app/assets/audio/Skeleton.mp3')
 class PlayBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: true,
+      loop: false,
+      duration: 0,
+      currentTime: 0,
+    };
+    this.stop = this.stop.bind(this);
+    this.play = this.play.bind(this);
+    this.loop = this.loop.bind(this);
+    this.handleMetadata = this.handleMetadata.bind(this);
+    this.updateTime = this.updateTime.bind(this);
+    this.formatTime = this.formatTime.bind(this);
+    this.onSliderChange = this.onSliderChange.bind(this)
+    // this.time = this.time.bind(this)
+    this.handlePlay = this.handlePlay.bind(this);
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-          playing: false,
-          loop: false,
-          duration: 0,
-          currentTime:0
-        }
-        this.stop = this.stop.bind(this)
-        this.play = this.play.bind(this)
-        this.loop = this.loop.bind(this)
-        this.handleMetadata = this.handleMetadata.bind(this)
-        this.updateTime = this.updateTime.bind(this)
-        this.formatTime = this.formatTime.bind(this)
-        // this.time = this.time.bind(this)
-        this.handlePlay = this.handlePlay.bind(this)
-    }
-
-    // componentDidUpdate(){
-    //     this.setState({ title: this.props.currentSong.title})
-    // }
-
-    formatTime(time){
-
-      let thisTime =time
-      let minutes = time/60
-      let seconds = Math.floor(time%60)
-      return (thisTime > 60 ? Math.floor(minutes) : "0") + ":"+ (seconds < 10? "0"+seconds : seconds )
-    }
-
-    updateTime(){
+  onSliderChange(e){
+      // console.log(e.target.value)
       const audio = document.getElementById("audio");
-      this.setState({ currentTime: audio.currentTime })
-    }
+      this.setState({ currentTime: e.target.value})
+      audio.currentTime = e.target.value
+  }
 
-    handleMetadata() {
-      const audio = document.getElementById("audio");
-      this.setState({ duration: audio.duration })
-    }
+  formatTime(time) {
+    let thisTime = time;
+    let minutes = time / 60;
+    let seconds = Math.floor(time % 60);
+    return (
+      (thisTime > 60 ? Math.floor(minutes) : "0") +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  }
 
-    handlePlay(){
-      (this.state.playing ? this.stop() : this.play())
-      this.setState({ playing: !this.state.playing })
-      // console.log(this.state.playing)
-       }
+  updateTime() {
+    const audio = document.getElementById("audio");
+    const timeSlider = document.getElementById("time-slider")
+    this.setState({ currentTime: audio.currentTime });
+    timeSlider.value = this.state.currentTime
+  }
 
-    play() {
-        let audio = document.getElementById("audio")
-        audio.play()
-    }
+  handleMetadata() {
+    const audio = document.getElementById("audio");
+    this.setState({ duration: audio.duration });
+  }
 
-    stop(){
-        let audio = document.getElementById("audio")
-        audio.pause()
-        }
-    
-    loop() {
-      let audio = document.getElementById("audio")
-      audio.loop = !audio.loop
-      this.setState({ loop: !this.state.loop })
-    }
+  handlePlay() {
+    this.state.playing ? this.stop() : this.play();
+    this.setState({ playing: !this.state.playing });
+    // console.log(this.state.playing)
+  }
 
-    // time() {
-    //   let audio = document.getElementById("time-place")
-    //   // return audio.currentTime
-    //   audio.innerHTML= this
+  play() {
+    let audio = document.getElementById("audio");
+    audio.play();
+  }
 
+  stop() {
+    let audio = document.getElementById("audio");
+    audio.pause();
+  }
+
+  loop() {
+    let audio = document.getElementById("audio");
+    audio.loop = !audio.loop;
+    this.setState({ loop: !this.state.loop });
+  }
+
+  // time() {
+  //   let audio = document.getElementById("time-place")
+  //   // return audio.currentTime
+  //   audio.innerHTML= this
+
+  // }
+  // componentDidUpdate() {
+
+  //     let audio = document.getElementById("audio")
+  //     audio.play()
+  // }
+
+  render() {
+    // if(this.props.currentTrack === null) {
+    //     return null
     // }
-    // componentDidUpdate() {
-        
-    //     let audio = document.getElementById("audio")
-    //     audio.play()
-    // }
+
+        // let { currentTrack } = this.props.currentTrack
+        // console.log(currentTrack, artist)
+
+    return (
+      <div className="playbar-div">
+        <div className="playbar-inner" >
+
+        <div className="media-controls">
+          <div>
+            <i className="fas fa-fast-backward"></i>
+          </div>
+          <div className="play-pause" onClick={() => this.handlePlay()}>
+            {this.state.playing ? (
+              <i className="fas fa-pause"></i>
+              ) : (
+                <i className="fas fa-play"></i>
+                )}
+          </div>
+          <div>
+            <i className="fas fa-fast-forward"></i>
+          </div>
+          <div
+            className={this.state.loop ? "orange-loop" : "loop"}
+            onClick={() => this.loop()}
+            >
+            {this.state.loop ? (
+              <i className="fas fa-redo-alt"></i>
+              ) : (
+                <i className="fas fa-redo-alt"></i>
+                )}
+          </div>
+
+        </div>
+
+        <div className="time-bar" >
+
+          <p> {this.formatTime(this.state.currentTime)}</p>
+        <audio
+        autoPlay
+        onTimeUpdate={this.updateTime}
+        onLoadedMetadata={this.handleMetadata}
+        id="audio"
+        // controls
+        src="https://docs.google.com/uc?export=download&id=1EZ83lkITMMggPrUoZLZOB7N5kqbN95cB"
+        // src={currentTrack.track_url}
+        >
+          {/* Your browser does not suppot rthe */}
+        </audio>
+        <input 
+        onChange={this.onSliderChange}
+        id="time-slider"
+        className="time-slider" 
+        type="range"
+        min="0"
+        // defaultValue="1.5"
+        max={this.state.duration}
+        />
+          <p> {this.formatTime(this.state.duration)}</p>
+          </div>
 
 
-    render(){
+        <div className="play-bar-song-info">
+          <img
+            src={
+              "https://cdn.spindizzyrecords.com/uploads/2017/07/default-release-cd.png"
+            }
+            />
 
-        // if(this.props.currentTrack === null) {
-        //     return null
-        // }
-
-        //     let { currentTrack, artist } = this.props.currentTrack
-        //     console.log(currentTrack, artist)
-
-            return (
-              <div className="playbar-div">
-
-              <div className="media-controls" >
-
-                <div>
-                <i className="fas fa-fast-backward"></i>
-                </div>
-                <div className="play-pause" onClick={() => this.handlePlay()} >
-                    {this.state.playing ? <i className="fas fa-pause"></i> : <i className="fas fa-play"></i>}
-                </div>
-                <div >
-                <i className="fas fa-fast-forward"></i>
-                </div>
-                <div className={this.state.loop ? "orange-loop" : "loop"} onClick={()=>this.loop()} >
-                  {this.state.loop ? <i className="fas fa-redo-alt"></i> : <i className="fas fa-redo-alt"></i>}
-                </div>
-
-              <div className="time" >
-                <span id="time-place">
-                  <p> {this.formatTime(this.state.currentTime)} 
-                  </p>
-                  <p> { (this.state.duration > 60 ? Math.floor(this.state.duration/60) : "0") + ":"+ Math.floor(this.state.duration)% 60 } </p>
-
-                  {/* {document.getElementById("audio").currentTime || null } */}
-                  </span>
-              </div>
-              </div>
-                {/* <input type="button" value="PLAY" onClick={() => this.handlePlay()} /> */}
-                {/* <input type="button" value="STOP" onClick={() => this.stop()} /> */}
-
-                <audio
-                onTimeUpdate={this.updateTime}
-                  onLoadedMetadata={this.handleMetadata}
-                // onTimeUpdate={document.getElementById('time-place').innerHTML = Math.floor(this.currentTime) + ' / ' + Math.floor(this.duration)}
-                  id="audio"
-                  controls
-                  src="https://docs.google.com/uc?export=download&id=1EZ83lkITMMggPrUoZLZOB7N5kqbN95cB"
-                  // src={currentTrack.track_url}
-                 >
-                  {/* Your browser does not suppot rthe */}
-                  {/* <code>audio</code> element. */}
-                </audio>
-                <div className="play-bar-song-info">
-
-                <img src={"https://cdn.spindizzyrecords.com/uploads/2017/07/default-release-cd.png"} />
-
-                <div className="artist-song-links">
-                <h1 >{"artist here"}</h1>
-                    <h1 >{"song here"}</h1>
-                    {/* <h1 >{currentTrack.artist || "artist here"}</h1>
+          <div className="artist-song-links">
+            <h1>{"artist here"}</h1>
+            <h1>{"song here"}</h1>
+            {/* <h1 >{currentTrack.artist || "artist here"}</h1>
                     <h1 >{currentTrack.title || "song here"}</h1> */}
-                </div>
-                </div>
-              </div>
-            );
-    
-    }
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default PlayBar
+export default PlayBar;
