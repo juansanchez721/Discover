3c91cb8668a72d72a4ffb512249a25dcb1b8240e
Juan Sanchez <juansanchez721@outlook.com>
2021-04-23 14:51:33 -0400

    Fixed playing from pause on next/prev

--- frontend/reducers/playbar_reducer.js
+++ frontend/reducers/playbar_reducer.js
@@ -17,30 +17,34 @@ import {
 const playbarReducer = (state = defaultArg, action) => {
   let newState = Object.assign({}, state);
   switch (action.type) {
-    
     case PLAY_SONG:
       newState.currentTrack = action.song;
       newState.isPlaying = true;
       return newState;
 
     case PLAY_NEW_SONG:
-      debugger
-      newState.played.push(newState.currentTrack.id)
+      // debugger
+      newState.played.push(state.currentTrack.id);
+      newState.isPlaying = true;
       newState.currentTrack = action.track;
-      return newState
+      return newState;
 
     case PAUSE_SONG:
       newState.isPlaying = false;
       return newState;
 
     case PLAY_NEXT_SONG:
+      newState.isPlaying = true;
+
       newState.played.push(state.currentTrack.id);
       newState.queue.shift(); //remove song from queue
       newState.currentTrack = Object.values(action.track)[0];
       return newState;
 
     case PLAY_PREV_SONG:
-      debugger;
+      // debugger;
+      newState.isPlaying = true;
+
       //   newState.queue.unshift(parseInt(Object.keys(action.track)[0])); //remove song from queue
       newState.queue.unshift(state.currentTrack.id);
       newState.played.pop(); //remove song from played
