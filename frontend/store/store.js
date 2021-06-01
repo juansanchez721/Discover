6e31805def9aad95c08e4268e9f0e8ba2e353e1c
Juan Sanchez <juansanchez721@outlook.com>
2021-06-01 17:57:49 -0400

    fixed search icon

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -10,7 +10,7 @@ const configureStore = (preloadedState={}) => {
         preloadedState, 
         applyMiddleware(
             thunk, 
-            logger
+            // logger
             ))
 }
 
